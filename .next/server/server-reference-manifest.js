e277e1db60213b88fd8c9bf9d4aa1c0142597d7e
isource2025 <hexanet@gmail.com>
2025-04-21 08:05:19 -0300

    .

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"7mgcsV3j7LArg6AtYikA3EtUom7A54CgjpSP/71XlBU=\"\n}"
-=======
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"i8JJ6URV8v/LRwd0id9pgdGEUjn5z5JKjzJY5L5zFqM=\"\n}"
->>>>>>> de39c16 (Merge de main a rama emiliano)
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ClGjf+i6EtPlCt6lKS2Ls68ZqgPlNxh6x55xxxDRiVU=\"\n}"
