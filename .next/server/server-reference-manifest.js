99669c9738e2f867b22caaa3f5f6495534c8a528
isource2025 <hexanet@gmail.com>
2025-04-21 08:01:42 -0300

    Merge de main a rama emiliano

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"7mgcsV3j7LArg6AtYikA3EtUom7A54CgjpSP/71XlBU=\"\n}"
+=======
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"i8JJ6URV8v/LRwd0id9pgdGEUjn5z5JKjzJY5L5zFqM=\"\n}"
+>>>>>>> de39c16 (Merge de main a rama emiliano)
