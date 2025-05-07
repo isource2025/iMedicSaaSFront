042d87b778fa5eddb1f9ecd1e413f25a5e77bda6
Emilianowav <emidbiasi@gmail.com>
2025-05-07 12:49:32 -0300

    .

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"jBg4GaGApVV5JU2CsXCjk9q33AWD6AshLVxkwy0OoBg=\"\n}"
-=======
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ClGjf+i6EtPlCt6lKS2Ls68ZqgPlNxh6x55xxxDRiVU=\"\n}"
->>>>>>> 584acaccfce93bf026155b31264c922f82c76b5b
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"40bvV6+YWMeSjMUUH/v7LdlSJz5oAMjz935ZoSzVOiM=\"\n}"
