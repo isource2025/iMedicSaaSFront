b9a546afc83abd4e9d7fdf6a2a2d96320a026b7d
Emilianowav <emidbiasi@gmail.com>
2025-09-17 08:20:50 -0300

    arreglando build

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"xK3Sx/dPaqwgRkBuvna9MX+jYC9l+Mi8IzwFl63GlOo=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"Wh+DkrbdqDaKcpXvKtMh9y/rB0ulkhcPTu4qPV3ZKtI=\"}"
