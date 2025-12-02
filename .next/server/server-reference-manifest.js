a7017f80e20ecc892c46764fe462b2d4aa27e86a
Humberto <humbertorbw@gmail.com>
2025-12-01 21:45:40 -0400

    chore: remove .next folder from version control

--- .next/server/server-reference-manifest.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"OI/GaYyvTFOsS9T1fOGV/AKOIwQjQyc6PI5T9QRdPR4=\"\n}"
-=======
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"oJA0tJSqjazzg5oVPXuZ3fs7bA6rxDs+6/rB6DiZicA=\"\n}"
->>>>>>> d647ef1114c5924db49448c5eeb8966ec7e8647f
