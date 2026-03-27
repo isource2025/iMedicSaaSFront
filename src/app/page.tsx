5641c2f7e8e9ec8873f00429551941d71b4f50ce
Emilianowav <emidbiasi@gmail.com>
2026-03-27 08:43:09 -0300

    Ajustar login: mostrar carrusel en desktop (50/50) y solo formulario en mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import LoginForm from './components/Login/LoginForm';
 import styles from './page.module.css';
+import LoginCarousel from './components/Carousel/LoginCarousel';
 
 export default function Home() {
   return (
     <main className={styles.loginPage}>
       <div className={styles.loginGrid}>
+        <div className={styles.imageSection}>
+          <LoginCarousel />
+        </div>
         <div className={styles.formSection}>
           <span className={styles.loginNote}>
             Software iMedic desarrollado por{' '}
